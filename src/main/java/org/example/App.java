143435916daffdf82974043525c23049a1a29d3a
sari <saritasuarez8028@gmail.com>
2022-05-04 11:36:34 -0500

    another push

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,19 +3,22 @@ package org.example;
 import org.onlab.packet.DHCP;
 import org.onlab.packet.Ip4Address;
 
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws SocketException {
         DHCP server =  new DHCP();
-        Ip4Address ip = Ip4Address.makeMaskPrefix(29);
+        DatagramSocket data = new DatagramSocket();
+        Ip4Address ip = Ip4Address.makeMaskPrefix(30);
         Ip4Address ip1 = Ip4Address.valueOf("192.168.0.1");
-        server.serialize();
-        System.out.println( ip1.toInt());
+
+        System.out.println( ip.toString());
     }
 
 }
